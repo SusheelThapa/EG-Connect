bc069cca83031db055096efaac4e364415d0f5bf
Rajendrakhanal <khanal.rajendra111@gmail.com>
2023-12-26 02:03:24 +0545

    Updated serializers.py file

--- backend/petition/serializers.py
+++ backend/petition/serializers.py
@@ -1,17 +1,17 @@
 from rest_framework import serializers
-from .models import User, Petition, Article, Signature, Comment
+from .models import User, Petition, Signature
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['id', 'username', 'email', 'password', 'registration_date']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
 
 class PetitionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Petition
-        fields = '__all__'
-
-class SignatureSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Signature
-        fields = '__all__'
+        fields = ['id', 'user', 'title', 'description', 'creation_date', 'status', 'target_signatures']
