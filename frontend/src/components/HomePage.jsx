9f7d67fbba94f389c63868cd49d6589ad4770e52
Saurav Kumar Mahato <077bct079.saurav@pcampus.edu.np>
2023-12-26 02:56:01 +0545

    Added props in header and body section

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -2,11 +2,17 @@ import Footer from "./Footer";
 import Header from "./Header";
 import Body from "./Body";
 
+import { useState } from "react";
+
 const HomePage = () => {
+
+  const [showPetitionContent, setShowPetitionContent] = useState(false)
+
+
   return (
     <>
-      <Header />
-      <Body />
+      <Header setShowPetitionContent={setShowPetitionContent} />
+      <Body showPetitionContent={showPetitionContent} />
       <Footer />
     </>
   );
