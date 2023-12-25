4a4d3a672ebeab6c1b96950a4e97bdd8fa084abc
Rajendrakhanal <khanal.rajendra111@gmail.com>
2023-12-26 02:48:38 +0545

    Api syntax changed from Petition

--- backend/petition/urls.py
+++ backend/petition/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('api/user/', views.UserAPIView.as_view(), name='petition-list'),
-    path('api/Petition/', views.PetitionAPIView.as_view(), name='article-list'),
+    path('api/petition/', views.PetitionAPIView.as_view(), name='article-list'),
     path('api/signature/', views.SignatureAPIView.as_view(), name='article-list'),
 ]
