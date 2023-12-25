ff1e3d72ce457562c852daf59a2747d4dd1956b4
Rajendrakhanal <khanal.rajendra111@gmail.com>
2023-12-26 03:33:47 +0545

    Changes made to admin.py

--- backend/petition/admin.py
+++ backend/petition/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, Petition, Signature
+
+# Register your models here.
+admin.site.register(User)
+admin.site.register(Petition)
+
+admin.site.register(Signature)
