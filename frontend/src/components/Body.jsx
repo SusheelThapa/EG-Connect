7cce7279a86c2200a173bbeb4788c0553ed81702
Saurav Kumar Mahato <077bct079.saurav@pcampus.edu.np>
2023-12-26 02:53:53 +0545

    corrected warning added propTypes to remove it

--- frontend/src/components/Body.jsx
+++ frontend/src/components/Body.jsx
@@ -1,10 +1,16 @@
-const Body = () => {
+import PropTypes from 'prop-types';
+
+const PetitionContent = () => {
   return (
-    <>
+    <div>
       <div className="grid grid-cols-2 gap-4">
         {/*  Row 1  */}
         <div className="border border-solid shadow-xl rounded-lg p-8 mx-10 my-3 flex flex-col">
-          <p className="text-xl font-serif  mb-4">Hello world To position the  button at the bottom right corner within your div, you can use the flex utility classes along with justify-end and items-end. Heres an updated version of your code:</p>
+          <p className="text-xl font-times  mb-4">
+            Hello world To position the button at the bottom right corner within
+            your div, you can use the flex utility classes along with
+            justify-end and items-end. Heres an updated version of your code:
+          </p>
           <div className="flex-grow"></div>
           <div className="flex justify-end items-end">
             <button className="rounded-lg bg-green-400 border border-solid px-2 py-1">
@@ -14,7 +20,7 @@ const Body = () => {
         </div>
 
         <div className="border border-solid shadow-xl rounded-lg p-8 mx-10 my-3 flex flex-col">
-          <p className="text-xl font-serif  mb-4">Hello world</p>
+          <p className="text-xl font-times  mb-4">Hello world</p>
           <div className="flex-grow"></div>
           <div className="flex justify-end items-end">
             <button className="rounded-lg bg-green-400 border border-solid px-2 py-1">
@@ -25,7 +31,7 @@ const Body = () => {
 
         {/* Row 2 */}
         <div className="border border-solid shadow-xl rounded-lg p-8 mx-10 my-3 flex flex-col">
-          <p className="text-xl font-serif  mb-4">Hello world</p>
+          <p className="text-xl font-times  mb-4">Hello world</p>
           <div className="flex-grow"></div>
           <div className="flex justify-end items-end">
             <button className="rounded-lg bg-green-400 border border-solid px-2 py-1">
@@ -35,7 +41,7 @@ const Body = () => {
         </div>
 
         <div className="border border-solid shadow-xl rounded-lg p-8 mx-10 my-3 flex flex-col">
-          <p className="text-xl font-serif  mb-4">Hello world</p>
+          <p className="text-xl font-times  mb-4">Hello world</p>
           <div className="flex-grow"></div>
           <div className="flex justify-end items-end">
             <button className="rounded-lg bg-green-400 border border-solid px-2 py-1">
@@ -46,8 +52,78 @@ const Body = () => {
 
         {/* <!-- Add more rows as needed --> */}
       </div>
-    </>
+    </div>
   );
 };
+
+const OtherContent = () => {
+  return (
+    <div>
+    <div className="grid grid-cols-2 gap-4">
+      {/*  Row 1  */}
+      <div className="border border-solid shadow-xl rounded-lg p-8 mx-10 my-3 flex flex-col">
+          <p className="text-xl font-times mb-4">
+            Petition the button at the bottom right corner within your div, you
+            can use the flex utility classes along with justify-end and items-end.
+            Here an updated version of your code:
+          </p>
+          <div className="flex-grow"></div>
+          <div className="flex justify-end items-end">
+            <button className="rounded-lg bg-green-400 border border-solid px-2 py-1">
+              SUPPORT
+            </button>
+          </div>
+        </div>
+
+      <div className="border border-solid shadow-xl rounded-lg p-8 mx-10 my-3 flex flex-col">
+        <p className="text-xl font-times  mb-4">Hello world</p>
+        <div className="flex-grow"></div>
+        <div className="flex justify-end items-end">
+          <button className="rounded-lg bg-green-400 border border-solid px-2 py-1">
+            SUPPORT
+          </button>
+        </div>
+      </div>
+
+      {/* Row 2 */}
+      <div className="border border-solid shadow-xl rounded-lg p-8 mx-10 my-3 flex flex-col">
+        <p className="text-xl font-times  mb-4">Hello world</p>
+        <div className="flex-grow"></div>
+        <div className="flex justify-end items-end">
+          <button className="rounded-lg bg-green-400 border border-solid px-2 py-1">
+            SUPPORT
+          </button>
+        </div>
+      </div>
+
+      <div className="border border-solid shadow-xl rounded-lg p-8 mx-10 my-3 flex flex-col">
+        <p className="text-xl font-times  mb-4">Hello world</p>
+        <div className="flex-grow"></div>
+        <div className="flex justify-end items-end">
+          <button className="rounded-lg bg-green-400 border border-solid px-2 py-1">
+            SUPPORT
+          </button>
+        </div>
+      </div>
+
+      {/* <!-- Add more rows as needed --> */}
+    </div>
+  </div>
+  );
+};
+
+const Body = ({ showPetitionContent }) => {
+  return (
+    <div className="p-4">
+      {showPetitionContent ? <PetitionContent /> : <OtherContent />}
+    </div>
+  );
+};
+
+// Add PropTypes validation
+Body.propTypes = {
+  showPetitionContent: PropTypes.bool.isRequired,
+};
+
 
 export default Body;
