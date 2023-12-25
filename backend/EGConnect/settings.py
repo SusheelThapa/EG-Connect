860793eb1f29031c7a5e44c7f7ceaa0b1c9d8893
Sandesh-Pyakurel <pyakurelsandesh14@gmail.com>
2023-12-26 04:25:17 +0545

    Cors header fixed:

--- backend/EGConnect/settings.py
+++ backend/EGConnect/settings.py
@@ -123,3 +123,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5174",
+    "http://127.0.0.1:5174",
+]
+
+CORS_ORIGIN_ALLOW_ALL = True
