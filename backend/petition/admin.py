fdd271f4b577d75f08c8b28f152faefffd7bde86
Rajendrakhanal <khanal.rajendra111@gmail.com>
2023-12-26 05:19:00 +0545

    Added Notice api endpoint to admin site

--- backend/petition/admin.py
+++ backend/petition/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import User, Petition, Signature
+from .models import User, Petition, Signature , Notice
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Petition)
 admin.site.register(Signature)
+admin.site.register(Notice)
