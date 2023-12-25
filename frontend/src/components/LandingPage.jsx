ef45240e58d3562a78c6a724f7f098361168cde4
Saurav Kumar Mahato <077bct079.saurav@pcampus.edu.np>
2023-12-26 04:58:53 +0545

    route corrected from 'Getstarted' to /policies

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -1,5 +1,6 @@
 import { useRef, useEffect } from "react";
 import Typed from "typed.js";
+import {Link} from 'react-router-dom'
 
 const LandingPage = () => {
   const typed_text = useRef(null);
@@ -26,7 +27,9 @@ const LandingPage = () => {
     <div className="bg-transparent w-screen   h-screen text-white flex flex-col items-center justify-center">
       <h1 className="text-8xl m-10">
         <span className="text-green-500">EG</span>{" "}
-        <span className="text-white	bold underline decoration-green-500">Connect</span>
+        <span className="text-white	bold underline decoration-green-500">
+          Connect
+        </span>
       </h1>
       <div className="text-4xl  mb-5">
         Transforming Nation with{" "}
@@ -36,9 +39,11 @@ const LandingPage = () => {
         ></span>
         .
       </div>
-      <button className="rounded-[15px] bg-green-500 m-6 py-3 px-4 text-3xl font-semibold  font-family-poppins tracking-wider hover:bg-teal-700">
-       Get Started
-      </button>
+      <Link to="/policies">
+        <button className="rounded-[15px] bg-green-500 m-6 py-3 px-4 text-3xl font-semibold  font-family-poppins tracking-wider hover:bg-teal-700">
+          Get Started
+        </button>
+      </Link>
     </div>
   );
 };
