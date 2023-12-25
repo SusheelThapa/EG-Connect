2ab3cb67d4becc0f3f1c67d35385e6af2f94aac3
Saurav Kumar Mahato <077bct079.saurav@pcampus.edu.np>
2023-12-26 01:35:08 +0545

    made body section and added it

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,10 +1,12 @@
 import Footer from "./Footer";
 import Header from "./Header";
+import Body from "./Body";
 
 const HomePage = () => {
   return (
     <>
       <Header />
+      <Body />
       <Footer />
     </>
   );
