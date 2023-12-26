97f66b344d23e36b3c4cc9da6505619712a6439d
Susheel Thapa <83917129+SusheelThapa@users.noreply.github.com>
2023-12-26 13:25:05 +0545

    Add code documentation in backend

--- backend/petition/models.py
+++ backend/petition/models.py
@@ -4,19 +4,22 @@ from django.db import models
 # Create your models here.
 from django.db import models
 
+# Choices for the status of petitions and notices
 STATUS_CHOICES = [
-        ('draft', 'Draft'),
-        ('pending', 'Pending'),
-        ('approved', 'Approved'),
-        ('rejected', 'Rejected'),
-    ]
+    ("draft", "Draft"),
+    ("pending", "Pending"),
+    ("approved", "Approved"),
+    ("rejected", "Rejected"),
+]
 
+
+# Custom User model extending Django's AbstractUser
 class User(AbstractUser):
     pass
 
 
+# Model representing a petition or policy
 class Petition(models.Model):
-   
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField()
@@ -24,12 +27,13 @@ class Petition(models.Model):
     status = models.CharField(max_length=20, choices=STATUS_CHOICES)
     target_signatures = models.PositiveIntegerField()
     is_petition = models.BooleanField()
-    tags = models.ManyToManyField('Tag')
+    tags = models.ManyToManyField("Tag")
 
     def __str__(self) -> str:
-        return self.title
+        return self.title  # String representation showing the petition's title
 
-    
+
+# Model for tags associated with petitions
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
@@ -37,12 +41,15 @@ class Tag(models.Model):
         return self.name
 
 
+
+# Model representing a signature for a petition
 class Signature(models.Model):
     petition = models.ForeignKey(Petition, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     signature_date = models.DateTimeField(auto_now_add=True)
 
 
+# Model representing a notice
 class Notice(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
--- backend/petition/views.py
+++ backend/petition/views.py
@@ -3,57 +3,85 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import User, Petition, Signature, Notice
-from .serializers import UserRegisterationSerializer, PetitionSerializer, SignatureSerializer , PoliciesSerializer, NoticeSerializer, UserLoginSerializer, UserProfileSerializer
+from .serializers import (
+    UserRegisterationSerializer,
+    PetitionSerializer,
+    SignatureSerializer,
+    PoliciesSerializer,
+    NoticeSerializer,
+    UserLoginSerializer,
+    UserProfileSerializer,
+)
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from .renderers import UserRenderer
-# Create your views here.
+
+
+# Utility function to generate JWT tokens for a user
+
 
 def get_tokens_for_user(user):
-  refresh = RefreshToken.for_user(user)
-  return {
-      'refresh': str(refresh),
-      'access': str(refresh.access_token),
-  }
+    refresh = RefreshToken.for_user(user)
+    return {
+        "refresh": str(refresh),
+        "access": str(refresh.access_token),
+    }
 
 
+# View for user registration
 class UserRegistrationView(APIView):
-  renderer_classes = [UserRenderer]
-  def post(self, request, format=None):
-    serializer = UserRegisterationSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    user = serializer.save()
-    token = get_tokens_for_user(user)
-    return Response({'token':token, 'msg':'Registration Successful'}, status=status.HTTP_201_CREATED)
+    renderer_classes = [UserRenderer]
 
+    def post(self, request, format=None):
+        serializer = UserRegisterationSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token = get_tokens_for_user(user)
+        return Response(
+            {"token": token, "msg": "Registration Successful"},
+            status=status.HTTP_201_CREATED,
+        )
+
+
+# View for user login
 class UserLoginView(APIView):
-  renderer_classes = [UserRenderer]
-  def post(self, request, format=None):
-    serializer = UserLoginSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    username = serializer.data.get('username')
-    password = serializer.data.get('password')
-    user = authenticate(username=username, password=password)
-    if user is not None:
-      token = get_tokens_for_user(user)
-      return Response({'token':token, 'msg':'Login Success'}, status=status.HTTP_200_OK)
-    else:
-      return Response({'errors':{'non_field_errors':['Email or Password is not Valid']}}, status=status.HTTP_404_NOT_FOUND)
-    
+    renderer_classes = [UserRenderer]
 
+    def post(self, request, format=None):
+        serializer = UserLoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        username = serializer.data.get("username")
+        password = serializer.data.get("password")
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            token = get_tokens_for_user(user)
+            return Response(
+                {"token": token, "msg": "Login Success"}, status=status.HTTP_200_OK
+            )
+        else:
+            return Response(
+                {"errors": {"non_field_errors": ["Email or Password is not Valid"]}},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+
+# View for fetching user profile
 class UserProfileView(APIView):
-  renderer_classes = [UserRenderer]
-  permission_classes = [IsAuthenticated]
-  def get(self, request, format=None):
-    serializer = UserProfileSerializer(request.user)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+    renderer_classes = [UserRenderer]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, format=None):
+        serializer = UserProfileSerializer(request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
+# API view for handling petitions
 class PetitionAPIView(APIView):
     permission_classes = [IsAuthenticated]
+
     def get(self, request):
-        petitions = Petition.objects.filter(is_petition = True, status = "approved")
+        petitions = Petition.objects.filter(is_petition=True, status="approved")
         serializer = PetitionSerializer(petitions, many=True)
         return Response(serializer.data)
 
@@ -65,12 +93,11 @@ class PetitionAPIView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-
-
 class PoliciesAPIView(APIView):
     permission_classes = [IsAuthenticated]
+
     def get(self, request):
-        petitions = Petition.objects.filter(is_petition = False, status = "approved")
+        petitions = Petition.objects.filter(is_petition=False, status="approved")
         serializer = PoliciesSerializer(petitions, many=True)
         return Response(serializer.data)
 
@@ -80,12 +107,12 @@ class PoliciesAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
 
-    
 
+# API view for handling signatures
 class SignatureAPIView(APIView):
     permission_classes = [IsAuthenticated]
+
     def post(self, request):
         serializer = SignatureSerializer(data=request.data)
         if serializer.is_valid():
@@ -93,8 +120,11 @@ class SignatureAPIView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+# API view for handling notices
 class NoticeAPIView(APIView):
     permission_classes = [IsAuthenticated]
+
     def get(self, request):
         notices = Notice.objects.all()
         serializer = NoticeSerializer(notices, many=True)
